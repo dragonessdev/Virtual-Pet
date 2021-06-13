02d13c3eccd2449e8ddcacf912cbbd8bf7497a3d
RoboticWolfStudios <85847845+RoboticWolfStudios@users.noreply.github.com>
2021-06-13 13:47:44 -0700

    Add files via upload

---
+++ Virtual pet game (coding project).py
@@ -0,0 +1,11 @@
+name  = input('What will you name your pet?')
+print('Say hi to your new pet,' + name)
+play = 'You played a game with your pet and they are happy now! You may want to let them rest before playing again, though.'
+feed = 'You fed your pet! They are full.'
+bath = name + ' is clean now! They dont need another bath until 10 minutes pass.'
+#readme!!
+#Hello! Here are some instructions to the game...
+#'play' is to play with your pet
+#'feed' is to feed your pet
+# and 'bath' is to clean your pet! Just press F5 to begin gameplay!
+#Keep in mind, this game is in beta! Have fun!
